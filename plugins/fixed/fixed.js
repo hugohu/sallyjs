a07e16fe276a1438aae17a7d504d41099722200e
hugohu <511194948@qq.com>
2014-04-01 23:23:10 +0800

    fiexd 事件重新封装 包含 gotop 以及fixed.删除原来单独的goTop事件 ,有待商榷.不排除重新拆分的想法.

--- plugins/fixed/fixed.js
+++ plugins/fixed/fixed.js
@@ -7,27 +7,41 @@
 (function($) {
 	$.fn.fixed = function(options) {
 		this.each(function() {
-			var $this = $(this);
-			var _target = $this.attr("data-target") || "active";
-			var HEIGTH_THIS = $this.height();
-			var WIDTH_THIS = $this.width();
-			$this.wrap('<div style="width:' + WIDTH_THIS + 'px; height:' + HEIGTH_THIS + 'px;position:relative">' + '</div>');
-			var TOP = $this.offset().top;
-			var _timer;
-			$(document).scroll(function() {
-				clearTimeout(_timer);
-				_timer = setTimeout(function() {
-					var scrollTop = $(window).scrollTop();
-					if (scrollTop >= TOP) {
-						$this.addClass(_target);
-					} else {
-						$this.removeClass(_target);
-					}
-				}, 0);
-			})
+			var $this = $(this),
+				_target = $this.attr("data-target") || "active",
+				_event = $this.attr("data-event");
+			switch (_event) {
+				case "fixed":
+					HEIGTH_THIS = $this.height(),
+					WIDTH_THIS = $this.width(),
+					TOP = $this.offset().top;
+					//SETHTML
+					$this.wrap('<div style="width:' + WIDTH_THIS + 'px; height:' + HEIGTH_THIS + 'px;position:relative">' + '</div>');
+					break;
+				case "gotop":
+					$(this).on("click", function(e) {
+						$("html, body").animate({
+							scrollTop: 0
+						}, 120);
+						e.preventDefault();
+					});
+					var TOP = 0;
+			}
+
+			function setFiexd() {
+				var st = $(window).scrollTop();
+				if (st > TOP) {
+					$this.addClass(_target);
+				} else {
+					$this.removeClass(_target);
+				}
+			}
+			//event
+			$(document).scroll($.throttle(37, setFiexd));
 		});
 	};
 })(jQuery);
 jQuery(function() {
 	$("[data-event='fixed']").fixed();
-})
+	$("[data-event='gotop']").fixed();
+});
