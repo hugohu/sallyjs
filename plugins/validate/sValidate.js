0db4f7780dd0e3efc683c73961a349aa0d7d9931
hugohu <511194948@qq.com>
2014-05-12 23:41:25 +0800

    test

--- plugins/validate/sValidate.js
+++ plugins/validate/sValidate.js
@@ -10,110 +10,155 @@
  * tips
  *
  */
-jQuery.fn.sValidate = function (options) {
+jQuery.fn.sValidate = function(options) {
   var defaults = {};
   var options = $.extend(defaults, options);
-  this.each(function () {
+  this.each(function() {
 
     var $this = $(this),
       einput = $("input,textarea", $this),
-      required = $("input[required],textarea[required]", $this),
-      echeck = $("[data-type='required']", $this),
+      required = $("input[required],textarea[required],[data-type='required']", $this),
       esubmit = $this.find(":submit");
     // Validate obj length
-    var count = required.length + echeck.length;
-    //verification attr
+    var count = required.length;
+    //validation attr
     // var tem_input=document.createElement("input"),
     // 		noRequired= !("required" in tem_input),
     // 		Nopattern = !("pattern" in tem_input);
     var Validate = {
       //Processing method set class
-      verification: function (elem) {
-        var parent = elem.sparent || elem.parent();
+      validation: function(elem,setClass) {
+      var parent = elem.sparent || elem.parent();
         var boole = elem.boole;
-        //console.log(boole)
+         var setClass=parent.hasClass("focus");
         if (boole) {
-          parent.removeClass("error");
+          elem.attr("data-validation", 1);
+          if(setClass){
+              parent.removeClass("error");
           parent.addClass("completed");
+          }
         } else {
-          parent.addClass("error");
+          elem.attr("data-validation", 0);
+          if(setClass){
+                      parent.addClass("error");
           parent.removeClass("completed");
+          }
+        };
+      },
+      setClass: function(elem) {
+        
+        var boole = elem.boole;
+        if (boole && setClass) {
+          
+        } else {
+
           return false;
         }
       },
-      //Organizing text box
-      required: function (ele) {
-        var ele = ele || required
-        ele.each(function (index, elem) {
-          var _this = $(this);
+      addMod: function() {
+        var select=function(_this) {
+          var echecked = _this.find(":checked");
+          _this.boole = echecked.length;
+          _this.sparent = _this;
+          Validate.validation(_this);
+        }
+        var input =function(_this) {
           _this.pattern = _this.attr("pattern");
           _this.value = $.trim(_this.val());
           //required
           _this.boole = $.trim(_this.val());
-          Validate.verification(_this);
+          Validate.validation(_this);
           //pattern
           if (_this.boole && _this.pattern) {
             Validate.pattern(_this);
           }
-          //ajax
-          
-        });
+        }
+        return {
+          select: select,
+          input: input
+        }
       },
-      //select box
-      echecked: function (ele) {
-        var ele = ele || echeck;
-        ele.each(function (index, elem) {
+      //Organizing text box
+      required: function(ele) {
+        var ele = ele || required;
+        ele.each(function(index, elem) {
           var _this = $(this);
-          var echecked = _this.find(":checked");
-          _this.boole = echecked.length;
-          _this.sparent = _this;
-          Validate.verification(_this);
+          var clen=_this.children().length;
+          if ( clen == 0) {
+             Validate.addMod().input(_this);
+          } else {
+           Validate.addMod().select(_this);
+          }
+          //ajax
         });
       },
       //regexp 
-      pattern: function (elem) {
+      pattern: function(elem) {
         var reg = new RegExp(elem.pattern);
         var value = elem.value;
         elem.boole = reg.test(value);
         //console.log(elem.boole)
-        this.verification(elem);
+        this.validation(elem);
       },
-      init: function () {
+       tips:function(){
+        var html="";
+        var ele=$("[data-validation=0]", $this);
+        ele.each(function (index, elem) {
+          var _this=$(this);
+          var txt=_this.next(".tips").text() || _this.attr("data-tips") || ""
+            html+=txt+"<br />";
+        })
+        return html;
+      },
+      setState:function(){
+        this.required();
+        var vlen=$("[data-validation=1]",$this).length;
+        if(vlen==count){
+           $this.attr("data-state", 1);
+          esubmit.removeClass("disabled");
+        } else{
+          console.log(this.tips())
+         $this.attr("data-state", 0);
+          esubmit.addClass("disabled");
+        }
+      },
+      init: function() {
         // input event
         einput.on({
-          "focus": function () {
+          "focus": function() {
             var _this = $(this),
               parent = _this.parent();
             parent.addClass("focus");
           },
-          "blur": function (e) {
+          "blur": function(e) {
             var _this = $(this),
               isRequired = _this.attr("required"),
               isRadio = _this.is(":radio");
-            parent = _this.parent();
-            parent.removeClass("focus");
+            
             //
             if (isRequired) {
               Validate.required(_this);
             }
             //radio
             if (isRadio) {
-              Validate.echecked(parent);
+              Validate.required(parent);
             }
+            Validate.setState();
+             // end to removeClass
+            parent = _this.parent();
+           parent.removeClass("focus");
           }
+
+         
         });
         // echeck event
-        esubmit.on("click.Validate", function (e) {
-          Validate.required();
-          Validate.echecked();
-          var completed_len = $(".completed", $this).length;
-          if (completed_len == count) {
-            $this.data("state", 1);
-          } else {
-            $this.data("state", 0);
-            return false;
+        esubmit.on("click.Validate", function(e) {
+         var _this=$(this);
+         if(_this.hasClass("disabled")){
+          return false;
           }
         })
+          Validate.setState();
       }
     }
     Validate.init();
@@ -122,11 +167,11 @@ jQuery.fn.sValidate = function (options) {
 };
 
 //ajaxform
-jQuery.fn.ajaxsubmit = function (options) {
+jQuery.fn.ajaxsubmit = function(options) {
   var defaults = {};
   var options = $.extend(defaults, options);
   // options fn 
-  this.each(function () {
+  this.each(function() {
     //code...
     var set_data = options.data;
     var $this = $(this),
@@ -143,10 +188,10 @@ jQuery.fn.ajaxsubmit = function (options) {
   });
 };
 
-jQuery(function () {
+jQuery(function() {
   $("[data-event='validate']").sValidate();
   esubmit = $(":submit");
-  esubmit.on("click", function (e) {
+  esubmit.on("click", function(e) {
     var form = $(this).parents("form");
     var isok = form.data("state");
     if (isok) {
