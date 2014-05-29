ef92cf187490cc25a36b9d50cebed03278154d69
hugohu <511194948@qq.com>
2014-05-29 22:50:33 +0800

    grunt真是好东西啊

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
       },
       dist: {
         src: ['plugins/**/*.js'],
-        dest: 'assets/js/<%= pkg.name %>.js'
+        dest: 'assets/js/<%= pkg.name %>.js',
       }
     },
     uglify: {
@@ -17,10 +17,20 @@ module.exports = function(grunt) {
       },
       dist: {
         files: {
-          'assets/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+          '<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
         }
       }
+    },
+    cssmin: {
+  add_banner: {
+    options: {
+      banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+    },
+    files: {
+      '<%= pkg.name %>.min.css': ['plugins/**/*.css'] 
     }
+  }
+}
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -28,9 +38,9 @@ module.exports = function(grunt) {
   // grunt.loadNpmTasks('grunt-contrib-qunit');
   // grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
-
+grunt.loadNpmTasks('grunt-contrib-cssmin');
   //grunt.registerTask('test', ['jshint', 'qunit']);
 
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify','cssmin']);
 
 };
