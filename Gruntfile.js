847080d4934c5569cf146ac76fbc5047721a5875
huugle <511194948@qq.com>
2014-05-30 11:36:25 +0800

    fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,25 +22,20 @@ module.exports = function(grunt) {
       }
     },
     cssmin: {
-  add_banner: {
-    options: {
-      banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-    },
-    files: {
-      '<%= pkg.name %>.min.css': ['plugins/**/*.css'] 
+      add_banner: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        },
+        files: {
+          '<%= pkg.name %>.min.css': ['plugins/**/*.css']
+        }
+      }
     }
-  }
-}
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
-  // grunt.loadNpmTasks('grunt-contrib-qunit');
-  // grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
-  //grunt.registerTask('test', ['jshint', 'qunit']);
-
-  grunt.registerTask('default', ['concat', 'uglify','cssmin']);
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 
 };
