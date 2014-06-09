417cba74583fb7d0ad2cad13b6e75f031388e733
huugle <511194948@qq.com>
2014-06-09 12:13:10 +0800

    重新整理代码

--- plugins/carousel/carousel.js
+++ plugins/carousel/carousel.js
@@ -1,194 +1,229 @@
-/*
- * carousel 1.0
- * Copyright (c) 2013 Huugle  http://huugle.org/
- * Date: 2014-03-13
- * 代码完全重构
- * 焦点图片切换
- * 参数说明
- * auto 配置是否滚动 默认值 true 可用值false
- * outTime 配置轮播切换时间 默认 4000ms 可以更改为任意数字
- * ebox 配置进行轮播的主体 默认为第一个子元素.可以自己任意更改为
- * class
- *
- * 配置方法 设置元素的 data-set='{"key":"value"}' json格式即可.
- *
- *
- */
-
 (function(factory) {
-	if (typeof define === "function" && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(["jquery"], factory);
-	} else {
-		// Browser globals
-		factory(jQuery);
-	}
-}(function($) {
-	// 这里放模块代码
-	$.fn.carousel = function(options) {
-		return this.each(function() {
-			//code
-			var $this = $(this),
-				_WIDTH = $this.width(),
-				edata = $this.attr("data-set"),
-				oSetOptions = {};
-			edata && (oSetOptions = $.parseJSON(edata));
-			// set options
-			var auto = oSetOptions.auto == "false" ? false : true,
-				outTime = parseInt(oSetOptions.outTime) || 4000,
-				ebox = oSetOptions.ebox ? $this.find(oSetOptions.ebox) : $this.children().first(),
-				edot = oSetOptions.edot,
-				emod = ebox.children(),
-				emod_len = emod.size(),
-				eWidth = emod.width(),
-				emod_width = eWidth * emod_len,
-				plen = Math.floor(_WIDTH / eWidth),
-				_moveWidth = plen * eWidth,
-				nlen = Math.ceil(emod_len / plen);
+		if (typeof define === "function" && define.amd) {
+			// AMD. Register as an anonymous module.
+			define(["jquery"], factory);
+		} else {
+			// Browser globals
+			factory(jQuery);
+		}
+	}(function($) {
 
-			if (nlen <= 1) {
-				return;
-			}
-			// set html		
-			if (edot) {
-				html = html = '<i class="dir prev hide" data-dir="-"></i><i class="dir next" data-dir="+"></i>';
-			} else {
-				var html = '<i class="dir prev hide" data-dir="-"></i><i class="dir next" data-dir="+"></i><span class="dot"><i class="active" data-index="0"></i>';
-				for (var i = 1; i < nlen; i++) {
-					html += '<i data-index="' + i + '"></i>';
-				}
-				html += "</span>"
-			}
+		var Carousel = function($this, params) {
+			var _this = this;
 
-			$this.append(html);
-			var eprev = $this.find(".prev"),
-				enext = $this.find(".next"),
-				edot = $this.find(edot || ".dot").children();
+			_this.$this = $this;
+			_this.index = 0;
+			_this.dataSet = _this.$this.attr("data-set");
+			_this.set = _this.dataSet ? $.parseJSON(_this.dataSet) : {};
+			//设置默认参数
+			var defaults = {
+				width: _this.$this.width(),
+				auto: true,
+				outTime: 4000,
+				ebox: _this.$this.children().first(),
+				edot: ".dot",
+				eprev: ".prev",
+				enext: ".next",
+				hasDot: false,
+				hasArr: false,
+				easyPlay: false
+			};
+			_this.params = $.extend(defaults, params || _this.set);
 
-			var carousel = {
-				index: 0,
-				play: function(arr, ind) {
-					var index = this.setIndex(arr, ind);
+			//set 程序运行需要的参数
+			_this.slideBox =_this.$this.find(_this.params["ebox"]).children();
+			_this.slideBoxLen = _this.slideBox.size();
+			_this.slideBoxWidth = _this.slideBox.width();
+			_this.slideBoxAllWidth = _this.slideBoxWidth * _this.slideBoxLen;
+			_this.pageLen = Math.floor(_this.params["width"] / _this.slideBoxWidth);
+			_this.moveWidth = _this.pageLen * _this.slideBoxWidth;
+			_this.pageCount = Math.ceil(_this.slideBoxLen / _this.pageLen);
 
-					//计算移动长度
-					if (index == nlen - 1) {
-						var dis = _WIDTH - emod_width;
-					} else {
-						var dis = -_moveWidth * index;
+
+			// 设置箭头
+			_this.setCarousel = function(Temp) {
+				var html = "";
+				if (!_this.params["hasDot"]) {
+					html += '<span class="dot">\n' +
+						'<i class="active" data-index="0"></i>\n'
+					for (var i = 1; i < _this.pageCount; i++) {
+						html += '<i data-index="' + i + '"></i>\n';
 					}
-					//高级浏览器开启硬件加速
-					var isIE9_ = document.all && !window.atob;
-					if (isIE9_) {
-						ebox.animate({
-							"margin-left": dis
-						}, 350)
+					html += "</span>";
+				}
+				if (!_this.params["hasArr"]) {
+					html += '<i class="dir prev hide" data-dir="-"></i>\n' +
+						'<i class="dir next" data-dir="+"></i>'
+				}
+				$(html).appendTo(_this.$this);
+				_this.edot = _this.$this.find(_this.params["edot"]).children();
+				_this.eprev = _this.$this.find(_this.params["eprev"]);
+				_this.enext = _this.$this.find(_this.params["enext"]);
+			}
+			//
+			_this.play= function(arr, ind) {
+				var index = _this.setIndex(arr, ind);
+				var isIE9_ = document.all && !window.atob;
+				//计算移动长度
+				if (index == _this.pageCount - 1) {
+					var dis = _this.params["width"] - _this.slideBoxAllWidth;
+				} else {
+					var dis = -_this.moveWidth * index;
+				}
+				if (_this.isNotSupportTranslate()) {
+					_this.params["ebox"].animate({
+						"margin-left": dis
+					}, 350)
+				} else {
+					
+					if (_this.params["easyPlay"]) {
+						var el = _this.params["ebox"][0];
+						_this.setTranslate(el, dis);
 					} else {
-						var eli = ebox.children();
-						var delayLong = 35 * eli.length;
-						var translate = 'translate(' + dis + 'px,0px)  translateZ(0px)';
-						eli.each(function(index, elem) {
+						var delayLong = 35 * _this.slideBoxLen;
+						_this.slideBox.each(function(index, elem) {
 							if (arr == "-") {
 								delay = delayLong - 35 * index;
 							} else {
 								delay = (35 * index) > delayLong ? delayLong : (35 * index);
 							}
-							delay+="ms";
-							var _this = $(this);
-							//内容不多 所以直接处理
-							_this.css({
-									'-webkit-transform': translate,
-									'-moz-transform': translate,
-									'-ms-transform': translate,
-									'-o-transform': translate,
-									'transform': translate,
-									'-webkit-transition-delay': delay,
-									'-moz-transition-delay': delay,
-									'-ms-transition-delay': delay,
-									'-o-transition-delay': delay,
-									'transition-delay': delay,
-								})
+							var that = $(this)[0];
+							_this.setTranslate(that, dis);
+							_this.setTransitionDelay(that, delay);
+
 						})
-					};
-					this.setActive(index);
-					!auto && this.hideButton(index);
-				},
-				setIndex: function(arr, ind) {
-					var index = this.index;
-					switch (arr) {
-						case "+":
-							if (index == nlen - 1) {
-								return this.index = 0;
-							} else {
-								return this.index += 1;
-							}
-							break;
-						case "-":
-							if (index == 0) {
-								return this.index = nlen - 1;
-							} else {
-								return this.index -= 1;
-							}
-							break;
-						case "click":
-							return this.index = ind;
-					}
-				},
-				setActive: function(index) {
-					edot.eq(index).addClass("active").siblings().removeClass("active");
-				},
-				autoPalay: function() {
-					this.loop = setInterval(function() {
-						carousel.play("+");
-					}, outTime)
-				},
-				stop: function() {
-					clearInterval(carousel.loop);
-				},
-				hideButton: function(index) {
-					if (index == nlen - 1) {
-						enext.addClass("hide");
-						eprev.removeClass("hide");
-					} else if (index == 0) {
-						eprev.addClass("hide");
-						enext.removeClass("hide");
-					} else {
-						enext.removeClass("hide");
-						eprev.removeClass("hide");
+
 					}
 
-				},
-				init: function() {
-					if (auto) {
-						this.autoPalay();
-						//hover
-						$this.hover(function() {
-							carousel.stop();
-						}, function() {
-							carousel.loop = setInterval(function() {
-								carousel.play("+");
-							}, outTime);
+				}
+				//others
+				_this.setActive(index);
+				!_this.params["auto"] && _this.hideButton(index);
+			}
+
+			_this.setIndex = function(arr, ind) {
+				var index = _this.index;
+				switch (arr) {
+					case "+":
+						if (index == _this.pageCount - 1) {
+							return _this.index = 0;
+						} else {
+							return _this.index += 1;
+						}
+						break;
+					case "-":
+						if (index == 0) {
+							return _this.index = _this.pageCount - 1;
+						} else {
+							return _this.index -= 1;
+						}
+						break;
+					case "click":
+						return _this.index = ind;
+				}
+			}
+
+			_this.setActive = function(index) {
+				_this.edot.eq(index).addClass("active").siblings().removeClass("active");
+			}
+			_this.autoPlay = function() {
+				_this.stopPlay();
+				_this.loop = setInterval(function() {
+					_this.play("+");
+				}, _this.params["outTime"])
+			}
+			_this.stopPlay= function() {
+				clearInterval(_this.loop);
+			}
+			_this.hideButton = function(index) {
+				if (index == _this.pageCount - 1) {
+					_this.enext.addClass("hide");
+					_this.eprev.removeClass("hide");
+				} else if (index == 0) {
+					_this.eprev.addClass("hide");
+					_this.enext.removeClass("hide");
+				} else {
+					_this.enext.removeClass("hide");
+					_this.eprev.removeClass("hide");
+				}
+			}
+			_this.init = function() {
+				if (_this.pageCount <= 1) {
+					return;
+				}
+				_this.setCarousel();
+				//添加事件
+				if (_this.params["auto"]) {
+					_this.autoPlay();
+					_this.$this.on({
+							"mouseover": function() {
+								_this.stopPlay();
+							},
+							"mouseout": function() {
+								_this.autoPlay();
+							}
 						});
 					}
+
 					// next
-					enext.on("click", function() {
-						carousel.play("+");
+					_this.enext.on("click", function() {
+						_this.play("+");
 					});
 					//next
-					eprev.on("click", function() {
-						carousel.play("-");
+					_this.eprev.on("click", function() {
+						_this.play("-");
 					});
 					//edot
-					edot.on("click", function() {
-						var $this = $(this);
+					_this.edot.on("click", function() {
 						var index = edot.index(this);
 						carousel.play("click", index);
 					})
 				}
+
+				_this.init();
 			}
-			carousel.init();
-			// end
-		});
-	};
-	//DATA API
-	$('[data-event="carousel"]').carousel();
-}));
+			Carousel.prototype = {
+				/*==================================================
+        Helpers
+   		====================================================*/
+				isNotSupportTranslate: function() {
+					var isIE9_ = document.all && !window.atob
+					return !!isIE9_;
+				},
+				getformat: function(template, json) {
+					return template.replace(/{{(.*?)}}/g, function(all, key) {
+						return json && (key in json) ? json[key] : "";
+					});
+				},
+				setTransform: function(el, transform) {
+					'use strict';
+					var es = el.style;
+					es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = transform;
+				},
+				setTranslate: function(el, dis) {
+					'use strict';
+					var es = el.style;
+					var transformString = 'translate(' + dis + 'px,0px)  translateZ(0px)'
+					es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = transformString;
+				},
+				setTransitionDuration: function(el, duration) {
+					'use strict';
+					var es = el.style;
+					es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = duration + 'ms';
+				},
+				setTransitionDelay: function(el, Delay) {
+					'use strict';
+					var es = el.style;
+					es.webkitTransitionDelay = es.MsTransitionDelay = es.msTransitionDelay = es.MozTransitionDelay = es.OTransitionDelay = es.transitionDelay = Delay + 'ms';
+				}
+			}
+			// code
+			$.fn.carousel = function(params) {
+				var s = new Carousel($(this), params);
+				$(this).data('swiper', s);
+				return s;
+			};
+			//DATA API
+			$('[data-event="carousel"]').carousel();
+			//return $.widget;
+		}));
