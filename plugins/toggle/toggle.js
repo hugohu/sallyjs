e4e935a8dadfcfa7fdab19710e83ac78ab4ee870
huugle <511194948@qq.com>
2014-07-09 11:00:41 +0800

    重新整改

--- plugins/toggle/toggle.js
+++ plugins/toggle/toggle.js
@@ -4,10 +4,10 @@
  * Date: 2014-06-04
  *
  *  思路说明:可以用addMethod的方法添加补充事件,比如
- *  $.stoggle.addMethod({name},{f})
+ *  $.stoggle({name},{f})
  *  其中name为模块名称,f为匿名函数.
  *  如果不是click事件,请在结尾处添加 $.stoggle.load({name});
- *  click事件直接写具体事件$.stoggle.addMethod({name},function($this,tar){ //code })
+ *  click事件直接写具体事件$.stoggle({name},function($this,tar){ //code })
  *  $this 为触发事件的元素
  *  tar为触发元素自定义属性 data-target 里的字符串.
  *  通常用于自定义切换样式等.
@@ -23,59 +23,56 @@
     factory(jQuery);
   }
 }(function($) {
-  $.stoggle = function(elements, options) {
-    $(elements).stoggle(options);
+  $.stoggle = function(eventanme, fn) {
+    $.stoggle[eventanme]=fn;
   };
-
   $.extend($.stoggle, {
-    addMethod: function(name, method) {
-      $.stoggle.methods[name] = method;
+    alert: function($this, e, tar) {
+      $this.parent().hide(350);
     },
-    methods: {
-      alert: function($this) {
-        $this.parent().hide(350);
-      },
-      actived: function($this, tar) {
-        $this.addClass(tar).siblings().removeClass(tar);
-      },
-      anchor: function($this) {
-        var href = $this.attr("href");
-        if (!href) {
-          return false;
-        }
-        var _id = $(href);
-        var TOP = parseInt(_id.offset().top) - 70;
-        $("html, body").animate({
-          scrollTop: TOP
-        }, 500);
-      },
-      tclass: function($this, tar) {
-        $this.toggleClass(tar);
-      },
-      closed: function($this,tar) {
-        $this.closest(tar).removeClass("active");
-      },
-      timeout:function(start,stop,delay){
-        start();
-        setTimeout(function(){
-        stop();
-        }, delay)
+    actived: function($this, e, tar) {
+      var _this = $(e.target)
+      _this.addClass(tar).siblings().removeClass(tar);
+    },
+    anchor: function($this, e) {
+      var _this = $(e.target);
+      var href = _this.attr("href");
+      if (!href) {
+        return false;
       }
+      var _id = $(href);
+      var TOP = parseInt(_id.offset().top) - 70;
+      $("html, body").animate({
+        scrollTop: TOP
+      }, 500);
+      e.preventDefault()
+    },
+    tclass: function($this, e, tar) {
+      $this.toggleClass(tar);
+    },
+    closed: function($this, e, tar) {
+      $this.closest(tar).removeClass("active");
+    },
+    timeout: function(start, stop, delay) {
+      start();
+      setTimeout(function() {
+        stop();
+      }, delay)
     },
     sMethods: [{
       type: "hover",
       motion: function($this, tar) {
-        $this.on("mouseover", function() {
-          clearTimeout($this.timeout);
-          $this.timeout = setTimeout(function() {
-            $this.addClass(tar);
-          }, 200);
-        }).on("mouseout", function() {
-          clearTimeout($this.timeout);
-          $this.timeout = setTimeout(function() {
-            $this.removeClass(tar);
-          }, 0);
-        });
+          $this.on("mouseover", function() {
+            clearTimeout($this.timeout);
+            $this.timeout = setTimeout(function() {
+              $this.addClass(tar);
+            }, 200);
+          }).on("mouseout", function() {
+            clearTimeout($this.timeout);
+            $this.timeout = setTimeout(function() {
+              $this.removeClass(tar);
+            }, 200);
+          });
       }
     }],
     load: function(s) {
@@ -83,9 +80,8 @@
         $('[data-toggle="' + s + '"]').stoggle();
       } else {
         //把需要直接载入的模块动态的添加到模块里然后执行直接执行
-        var amod = this.sMethods;
-        $.each(amod, function(i, n) {
-          $.stoggle.addMethod(n.type, n.motion);
+        $.each(this.sMethods, function(i, n) {
+          $.stoggle(n.type, n.motion);
           $.stoggle.load(n.type);
         });
       }
@@ -100,25 +96,18 @@
       var $this = $(this);
       var eventname = $this.attr("data-toggle");
       var tar = $this.attr("data-target") || "active";
-      $.stoggle.methods[eventname] && $.stoggle.methods[eventname]($this, tar);
+      $.stoggle[eventname] && $.stoggle[eventname]($this, tar);
     });
   };
 
   // start load
   $.stoggle.load();
   //add event
-  $(document).on("click", '[data-toggle]', function(e) {
-    $this = $(e.target);
-    var eventname = $this.attr("data-toggle"),
-      tar;
-    if (eventname) {
-      tar = $this.attr("data-target") || "active";
-    } else {
-      var parent = $(this);
-      eventname = parent.attr("data-toggle");
-      tar = parent.attr("data-target") || "active";
-    }
-    $.stoggle.methods[eventname] && $.stoggle.methods[eventname]($this, tar);
-    e.preventDefault();
+  $(document).on("click", '[data-toggle-click]', function(e) {
+
+    var $this = $(this);
+    var tar = $this.attr("data-target") || "active";
+    var eventname = $this.attr("data-toggle-click")
+    $.stoggle[eventname] && $.stoggle[eventname]($this, e, tar);
   });
 }));
