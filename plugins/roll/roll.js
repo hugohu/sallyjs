329f3e1320d3f64ff167e238fba13a77d5b46fdb
huugle <511194948@qq.com>
2014-10-23 15:25:17 +0800

    Update roll.js

--- plugins/roll/roll.js
+++ plugins/roll/roll.js
@@ -1,9 +1,6 @@
-/*
- * layer 0.1
- * Copyright (c) 2013 Huugle  http://huugle.org/
- * Date: 2014-01-21
- * layer
- */
+/* =========================================================
+ * roll load
+ * ========================================================= */
 
 (function(factory) {
   if (typeof define === "function" && define.amd) {
@@ -15,25 +12,87 @@
   }
 }(function($) {
   // code
-  $.throttle=function(a,b,c,d){function g(){function j(){f=+new Date,c.apply(g,i)}function k(){e=void 0}var g=this,h=+new Date-f,i=arguments;d&&!e&&j(),e&&clearTimeout(e),void 0===d&&h>a?j():b!==!0&&(e=setTimeout(d?k:j,void 0===d?a-h:a))}var e,f=0;return"boolean"!=typeof b&&(d=c,c=b,b=void 0),$.guid&&(g.guid=c.guid=c.guid||$.guid++),g};
-  
-  $.roll = function(eventanme, fn) {
-    $.roll[eventanme] = fn;
+  $.throttle = function(delay, no_trailing, callback, debounce_mode) {
+    // After wrapper has stopped being called, this timeout ensures that
+    // `callback` is executed at the proper times in `throttle` and `end`
+    // debounce modes.
+    var timeout_id,
+
+      // Keep track of the last time `callback` was executed.
+      last_exec = 0;
+
+    // `no_trailing` defaults to falsy.
+    if (typeof no_trailing !== 'boolean') {
+      debounce_mode = callback;
+      callback = no_trailing;
+      no_trailing = undefined;
+    }
+
+    // The `wrapper` function encapsulates all of the throttling / debouncing
+    // functionality and when executed will limit the rate at which `callback`
+    // is executed.
+    function wrapper() {
+      var that = this,
+        elapsed = +new Date() - last_exec,
+        args = arguments;
+
+      // Execute `callback` and update the `last_exec` timestamp.
+      function exec() {
+        last_exec = +new Date();
+        callback.apply(that, args);
+      };
+
+      // If `debounce_mode` is true (at_begin) this is used to clear the flag
+      // to allow future `callback` executions.
+      function clear() {
+        timeout_id = undefined;
+      };
+
+      if (debounce_mode && !timeout_id) {
+        // Since `wrapper` is being called for the first time and
+        // `debounce_mode` is true (at_begin), execute `callback`.
+        exec();
+      }
+
+      // Clear any existing timeout.
+      timeout_id && clearTimeout(timeout_id);
+
+      if (debounce_mode === undefined && elapsed > delay) {
+        // In throttle mode, if `delay` time has been exceeded, execute
+        // `callback`.
+        exec();
+
+      } else if (no_trailing !== true) {
+        // In trailing throttle mode, since `delay` time has not been
+        // exceeded, schedule `callback` to execute `delay` ms after most
+        // recent execution.
+        // 
+        // If `debounce_mode` is true (at_begin), schedule `clear` to execute
+        // after `delay` ms.
+        // 
+        // If `debounce_mode` is false (at end), schedule `callback` to
+        // execute after `delay` ms.
+        timeout_id = setTimeout(debounce_mode ? clear : exec, debounce_mode === undefined ? delay - elapsed : delay);
+      }
+    };
+
+    // Set the guid of `wrapper` function to the same of original callback, so
+    // it can be removed in jQuery 1.4+ .unbind or .die by using the original
+    // callback as a reference.
+    if ($.guid) {
+      wrapper.guid = callback.guid = callback.guid || $.guid++;
+    }
+
+    // Return the wrapper function.
+    return wrapper;
   };
-$.extend($.roll, {
-
-  
-})
-
-    jQuery.fn.roll = function(options) {
+  $.fn.roll = function(options) {
     var defaults = {};
     var options = $.extend(defaults, options);
-    return this.each(function() {
+    this.each(function() {
       var $this = $(this),
         _target = $this.attr("data-target") || "active",
         _event = options.event || $this.attr("data-roll");
-
-
       var build = {
         fixed: function() {
           var HEIGTH_THIS = $this.height(),
@@ -49,60 +108,53 @@ $.extend($.roll, {
           var dis = "";
           $this.dirs = $this.dirs ? $this.dirs : st;
           if (st < $this.dirs) {
-            dis = "up";
+            dis = "up"
           } else {
-            dis = "down";
+            dis = "down"
           }
           $this.dirs = st;
           return dis;
         },
         follow: function() {
           var ele = $("a", $this);
-          var obj = {};
+          var inViews = [];
           $this.data("index", 0);
-          obj.length = ele.length - 1;
           ele.each(function(index, elem) {
-            var _Id = $(this).attr("href")
-            var _TOP = parseInt($(_Id).offset().top) - 90;
-            obj[index] = {
-              index: index,
-              id: _Id,
-              top: _TOP
+            var _Id = $(this).attr("href");
+            if ($(_Id).length) {
+              inViews.push($(_Id));
             }
           });
 
-          function setActived(index) {
+          function setActived(id) {
             ele.removeClass("active");
-            ele.eq(index).addClass("active");
+            $("a[href=#" + id + "]").addClass("active");
           }
 
           function setfollow() {
-            var index = $this.data("index");
-            var inext = (index + 1 > obj.length) ? obj.length : index + 1;
-            var iprev = (index - 1 < 0) ? 0 : index - 1;
-            var _aNext = obj[inext];
-            var _aPrev = obj[iprev];
-            var _aTop = obj[index].top;
-
-            var st = $(window).scrollTop();
-            if (st > _aTop && st < _aNext.top) {
-              setActived(index);
-            } else if (st < _aTop && st > _aPrev.top) {
-              $this.data("index", _aPrev.index)
-              setActived(_aPrev.index);
-            } else if (st > _aNext.top) {
-              $this.data("index", _aNext.index);
-              setActived(_aNext.index);
+            var scrollTop = $(window).scrollTop();
+            if (inViews.length) {
+              var $target;
+              $.each(inViews, function(i, item) {
+                console.log(item.offset().top, scrollTop)
+                if (item.offset().top >= scrollTop) {
+                  $target = item;
+                  return false; // break
+                }
+              });
+              if (!$target) {
+                return;
+              }
+              var id = $target.attr("id");
+              setActived(id)
             }
-
-
           }
-          var delay = 37;
+          var delay = 50;
           this.start(delay, setfollow);
         },
         gotop: function() {
           $this.on("click", function(e) {
-            build.setTop();
+            build.setTop()
             e.preventDefault();
           });
           this.top = 600;
@@ -112,7 +164,7 @@ $.extend($.roll, {
         setTop: function() {
           $("html, body").animate({
             scrollTop: 0
-          }, 350);
+          }, 120);
         },
         setFiexd: function() {
           var st = $(window).scrollTop();
